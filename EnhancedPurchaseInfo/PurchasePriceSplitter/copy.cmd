108be72380503da749a01e37dae4a6ff674a84f1
DJ Doena <karsten@yajirobi.de>
2017-04-26 15:59:34 +0200

    1.1.3.0

---
+++ EnhancedPurchaseInfo/PurchasePriceSplitter/copy.cmd
@@ -0,0 +1,2 @@
+copy /y PurchasePriceSplitter.* ..\..\..\..\EnhancedPurchaseInfo\bin\x86\EnhancedPurchaseInfo\
+copy /y de\PurchasePriceSplitter.* ..\..\..\..\EnhancedPurchaseInfo\bin\x86\EnhancedPurchaseInfo\de\
