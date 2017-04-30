a9ced2e4dcca3f3630f32236878113d2cd1cf57e
DJ Doena <karsten@yajirobo.de>
2017-04-30 11:06:11 +0200

    1.1.3.1

--- EnhancedPurchaseInfo/EnhancedPurchaseInfo/Properties/EnhancedPurchaseInfo.AssemblyInfo.cs
+++ EnhancedPurchaseInfo/EnhancedPurchaseInfo/Properties/EnhancedPurchaseInfo.AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.1.3.0")]
-[assembly: AssemblyFileVersion("1.1.3.0")]
+[assembly: AssemblyVersion("1.1.3.1")]
+[assembly: AssemblyFileVersion("1.1.3.1")]
