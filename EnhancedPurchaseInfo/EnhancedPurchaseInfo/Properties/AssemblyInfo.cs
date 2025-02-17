533ac56e1f5cfc7f470ecb252537b62a48800066
DJ Doena <djdoena@outlook.com>
2025-02-17 20:49:35 +0100

    1.1.4

--- EnhancedPurchaseInfo/EnhancedPurchaseInfo/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: ComVisible(true)]
-[assembly: Guid("BF7AF847-CF55-4DD1-A544-A2F34502ACEB")]
-
-[assembly: AssemblyDelaySign(false)]
