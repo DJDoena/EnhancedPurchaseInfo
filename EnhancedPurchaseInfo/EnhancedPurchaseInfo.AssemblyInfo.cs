5e79e27915bc67e1542a0602055ba25fe079ed54
DJ Doena <djdoena@outlook.com>
2025-02-18 10:21:24 +0100

    clean-up

---
+++ EnhancedPurchaseInfo/EnhancedPurchaseInfo.AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Enhanced Purchase Info")]
+[assembly: AssemblyDescription("Allows to add more detailed information about the purchase (more price fields, more dates).")]
+
+[assembly: AssemblyCompany("Doena Soft.")]
+[assembly: AssemblyProduct("DVD Profiler to Access")]
+[assembly: AssemblyCopyright("Copyright © Doena Soft. 2017 - 2025")]
+
+[assembly: AssemblyVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
